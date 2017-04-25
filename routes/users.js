b2c613c466f17b7de46dee476cad666c994228d6
Keith Davis <kpdavis.kd@outlook.com>
2017-04-25 09:18:39 -0600

    signup form unstyled

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,13 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const pg = require('../db/knex');
+const router = express.Router();
 
 /* GET users listing. */
 router.get('/signup', function(req, res) {
-  res.render('signup');
+  res.render('signup', {layout: 'layout.hbs'});
 });
-router.post('/',function(req, res) {
+router.post('/profile',function(req, res) {
   console.log(req.body);
-} )
+});
 
 module.exports = router;
